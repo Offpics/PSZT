b2a7299b36bcd79770c0e87216c87175a5102035
Aquakor <kornelux1@gmail.com>
2018-12-22 16:42:14 +0100

    Add vectorize_dataset

--- refactor_csv.py
+++ refactor_csv.py
@@ -1,8 +1,11 @@
 import csv
+import numpy as np
+import re
+
 
 def refactor(csv_path, new_csv_path):
     """Extract sentiment and text into new csv file.
-    
+
     Args:
     csv_path -- file path to the csv file
     new_csv_path -- file path to the new csv file
@@ -22,12 +25,70 @@ def refactor(csv_path, new_csv_path):
                 # Write every 'sentiment' and 'text' to new row.
                 csv_writer.writerow([row[5], row[11]])
 
+
+def vectorize_dataset(csv_path):
+    """Perform bag of words algorithm on the dataset.
+    TODO: 
+    Right now this function only performs bag-of-words on the dataset 
+    and returns every sentence as vector in form of numpy array. 
+    We need to add label (sentiment) to every sentence but tbh I'm not sure
+    how the neural net model will look like. Also this function should
+    save the dataset on the disk because the time to compute this function
+    is pretty long.
+    """
+    # List of words in the dataset.
+    dataset_words = []
+
+    # List of sentences in the dataset.
+    dataset_sentences = []
+
+    # Open csv dataset.
+    with open(csv_path) as csv_file:
+        #Create reader object used to iterate over every row in the file.
+        csv_content = csv.reader(csv_file, delimiter=',')
+
+        for row in csv_content:
+            #TODO: Skip first row because it has labels in it.
+            ignore_words = ['a']
+            # Split every words into a list.
+            words = re.sub("[^\w]", " ",  row[1]).split()
+
+            # Clean words.
+            words = [w.lower() for w in words if w not in ignore_words]
+
+            dataset_words.extend(words)
+            dataset_sentences.append(words)
+
+    # Transform into set to avoid duplicates and create sorted list.
+    dataset_words = sorted(list(set(dataset_words)))
+
+    # Perform bag of words on the dataset.
+    for i, sentence in enumerate(dataset_sentences):
+        # Numpy array that will represent every sentence with frequencies
+        # of used words.
+        bag = np.zeros(len(dataset_words))
+
+        for word_s in sentence:
+            for j, word_w in enumerate(dataset_words):
+                if word_w == word_s:
+                    bag[j] += 1
+        
+        dataset_sentences[i] = bag
+
+    # Transform dataset_sentences list into numpy array.
+    a = np.array(dataset_sentences)
+    return a
+
+
 if __name__ == "__main__":
-    # File path to the csv file.
-    csv_path = 'Apple-Twitter-Sentiment-DFE.csv'
+    dataset = vectorize_dataset('apple-twitter2.csv')
+    print(len(dataset[2]))
 
-    # File path to the new csv file.
-    new_csv_path = 'apple-twitter.csv'
+    # # File path to the csv file.
+    # csv_path = 'Apple-Twitter-Sentiment-DFE.csv'
 
-    # Refactor dataset.
-    refactor(csv_path, new_csv_path);
+    # # File path to the new csv file.
+    # new_csv_path = 'apple-twitter.csv'
+
+    # # Refactor dataset.
+    # refactor(csv_path, new_csv_path)
