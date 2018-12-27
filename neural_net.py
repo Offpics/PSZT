0fdd6f0b22750c0ec98ad6b3c33d64ad083d8d70
Aquakor <kornelux1@gmail.com>
2018-12-27 15:55:48 +0100

    Add cross entropy loss to neural net

--- neural_net.py
+++ neural_net.py
@@ -1,5 +1,5 @@
 import numpy as np
-from activations import linear, sigmoid
+from activations import linear, sigmoid, relu, softmax
 
 class MLP:
     """ Multilayer perceptron model. """
@@ -10,7 +10,7 @@ class MLP:
             {"input_dim": 8257, "output_dim": 4000, "activation": linear},
             {"input_dim": 4000, "output_dim": 100, "activation": linear},
             {"input_dim": 100, "output_dim": 10, "activation": linear},
-            {"input_dim": 10, "output_dim": 3, "activation": sigmoid},
+            {"input_dim": 10, "output_dim": 3, "activation": softmax},
         ]
 
         self.param_values = {}
@@ -48,6 +48,20 @@ class MLP:
 
             self.memory["A" + str(idx-1)] = A_prev
             self.memory["Z" + str(idx)] = Z_curr
+
+        return A_curr
+
+
+    def cross_entropy_loss(self, y_pred, y_true):
+        cross_entropy = 0
+
+        for i in range(len(y_pred)):
+            cross_entropy += y_true[i] * np.log(y_pred[i])
+    
+        cross_entropy = -cross_entropy.sum()
+
+        return cross_entropy
+
             
 
     def add(self, input_dim, output_dim, activation):
