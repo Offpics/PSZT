8e11217e7462d870c86de3000b8fcfbd191df93f
Aquakor <kornelux1@gmail.com>
2019-01-15 21:35:41 +0100

    Update

--- pszt/net.py
+++ pszt/net.py
@@ -5,7 +5,8 @@ class MLP():
     """ Multilayer Perceptron.
 
     This artificial neural network model is designed for sentiment analysis.
-    The activation function of the last layer should always be 'softmax'. """
+    The activation function of the last layer should always be 'softmax'
+    and input layer should be ommited. """
 
     def __init__(self):
         # List of layers of the model.
@@ -28,6 +29,22 @@ class MLP():
 
         # Learning rate of the optimizer.
         self.lr = 10e-4
+
+    def add_layer(self, input_dim, output_dim, activation):
+        """ Add layer to the neural network.
+
+        Args:
+            input_dim: Dimension of the input.
+            output_dim: Dimension of the output.
+            activation: Activation function.
+        """
+
+        # Create new layer.
+        layer = {"input_dim": input_dim,
+                 "output_dim": output_dim,
+                 "activation": activation}
+
+        self.layers.append(layer)
 
     def init_layers(self):
         """ Initialize weights w, b in every layer of the neural network. """
@@ -65,14 +82,16 @@ class MLP():
                     size=output_size
                 )
 
-    def train(self, x, y_true, epochs, silent=False):
+    def train(self, x, y_true, x_test, y_test, epochs, silent=False):
         """ Perform training of neural network.
 
         Args:
-            x(numpy.ndarray): Array of input data in shape
-                              (num_of_inputs, vector_input).
+            x(numpy.ndarray): Training dataset of shape (num_of_inputs, vector_input).
             y_true(numpy.ndarray): Array of true labels in one-hot format
-                                   and shape (num_of_inputs, one-hot).
+                                    for training set of shape (num_of_inputs, one-hot).
+            x_test(numpy.ndarray): Test dataset of shape (num_of_inputs, vector_input).
+            y_test(numpy.ndarray): Array of true labels in one-hot format
+                                    for test set of shape (num_of_inputs, one-hot).
             epochs(int): Number of epochs to perform.
             silent(bool): Whether to calculate loss and accuracy and print it.
         """
@@ -88,28 +107,32 @@ class MLP():
             self._update_weights()
 
             # Calculate accuracy and loss of the trained network on train set.
-            accuracy, loss = self.score(x, y_true)
+            accuracy_train, loss_train = self.score(x, y_true)
 
-            # TODO: Add calculation of accuracy and loss on test set.
+            # Calculate accuracy and loss of the trained network on test set.
+            accuracy_test, loss_test = self.score(x_test, y_test, True)
 
             # Store current accuracy.
-            self.accuracies.append(accuracy)
-            self.losses.append(loss)
+            self.accuracies.append([accuracy_train, accuracy_test])
+            self.losses.append([loss_train, loss_test])
 
             if silent is False:
                 if i % 10 == 0:
                     # Print current loss and accuracy of the neural net.
-                    print(f'Epoch: {i}, loss: {loss:.2f}, accuracy: {accuracy:.1f}%')
+                    print(f'Epoch: {i}, loss_train: {loss_train:.2f}, accuracy_train: {accuracy_train:.1f}%')
+                    print(f'\t  loss_test: {loss_test:.2f}, accuracy_test: {accuracy_test:.1f}%')
 
-    def train_batch_with_test(self, x, y_true, x_test, y_test,
-                              epochs, silent=False, batch_size=64):
-        """ Perform training of neural network.
+    def train_minibatch(self, x, y_true, x_test, y_test,
+                                  epochs, silent=False, batch_size=64):
+        """ Perform training of neural network using minibatch.
 
         Args:
-            x(numpy.ndarray): Array of input data in shape
-                              (num_of_inputs, vector_input).
+            x(numpy.ndarray): Training dataset of shape (num_of_inputs, vector_input).
             y_true(numpy.ndarray): Array of true labels in one-hot format
-                                   and shape (num_of_inputs, one-hot).
+                                    for training set of shape (num_of_inputs, one-hot).
+            x_test(numpy.ndarray): Test dataset of shape (num_of_inputs, vector_input).
+            y_test(numpy.ndarray): Array of true labels in one-hot format
+                                    for test set of shape (num_of_inputs, one-hot).
             epochs(int): Number of epochs to perform.
             silent(bool): Whether to calculate loss and accuracy and print it.
             batch_size(int): Number of samples from dataset to train net.
@@ -117,7 +140,7 @@ class MLP():
 
         for i in range(1, epochs+1):
             for index in range(0, x.shape[0], batch_size):
-                # Create x, y batches.
+                # Create x, y minibatches.
                 x_batch = x[index:min(index+batch_size, x.shape[0])]
                 y_batch = y_true[index:min(index+batch_size, x.shape[0])]
                 
@@ -131,123 +154,97 @@ class MLP():
                 self._update_weights()
 
                 # Calculate accuracy and loss of the trained network on train set.
-                accuracy, loss = self.score(x, y_true, True)
+                accuracy_train, loss_train = self.score(x, y_true, True)
 
                 # Calculate accuracy and loss of the trained network on test set.
                 accuracy_test, loss_test = self.score(x_test, y_test, True)
 
             # Store current accuracy.
-            self.accuracies.append([accuracy, accuracy_test])
-            self.losses.append([loss, loss_test])
+            self.accuracies.append([accuracy_train, accuracy_test])
+            self.losses.append([loss_train, loss_test])
 
             if silent is False:
                 if i % 1 == 0:
                     # Print current loss and accuracy of the neural net.
-                    print(f'Epoch: {i}, loss_train: {loss:.2f}, accuracy_train: {accuracy:.1f}%')
+                    print(f'Epoch: {i}, loss_train: {loss_train:.2f}, accuracy_train: {accuracy_train:.1f}%')
                     print(f'\t  loss_test: {loss_test:.2f}, accuracy_test: {accuracy_test:.1f}%')
 
-    def train_batch(self, x, y_true, epochs, silent=False, batch_size=64):
-        """ Perform training of neural network.
-
+    def k_fold_validation(self, x, y_true, k, epochs):
+        """ Perform k-fold cross validation.
         Args:
             x(numpy.ndarray): Array of input data in shape
                               (num_of_inputs, vector_input).
             y_true(numpy.ndarray): Array of true labels in one-hot format
                                    and shape (num_of_inputs, one-hot).
+            k(int): Number of folds.
             epochs(int): Number of epochs to perform.
-            silent(bool): Whether to calculate loss and accuracy and print it.
-            batch_size(int): Number of samples from dataset to train net.
         """
 
-        for i in range(1, epochs+1):
-            for index in range(0, x.shape[0], batch_size):
-                # Create x, y batches.
-                x_batch = x[index:min(index+batch_size, x.shape[0])]
-                y_batch = y_true[index:min(index+batch_size, x.shape[0])]
-                
-                # Perform forward propagation over neural network.
-                self._forward(x_batch)
+        # Create a random order to shuffle the dataset.
+        s = np.arange(x.shape[0])
+        np.random.shuffle(s)
 
-                # Perform backward propagation over neural network.
-                self._backward(x_batch, y_batch)
+        # Split dataset into k folds.
+        x_folds = np.array_split(x[s], k)
+        y_folds = np.array_split(y_true[s], k)
 
-                # Update weights of neural network.
+        # Lists to store accuracies and losses in ith step.
+        accuracies = []
+        losses = []
+
+        for i in range(len(x_folds[:k])):
+            # Init new weights for layers.
+            self.init_layers()
+
+            # Create train test T\Ti.
+            x_train = np.concatenate(np.delete(x_folds, i))
+            y_train = np.concatenate(np.delete(y_folds, i))
+
+            # Create test set Ti.
+            x_test = x_folds[i]
+            y_test = y_folds[i]
+
+            # Lists to store accuracies and losses in jth step.
+            accuracies_curr = []
+            losses_curr = []
+
+            print(f'Current fold: T{i},')
+            print(f'Len of x_train: {len(x_train)},')
+            print(f'Len of x_test: {len(x_test)}.')
+
+            # Perform training.
+            for j in range(1, epochs+1):
+                # Perform training and update weights.
+                self._forward(x_train)
+                self._backward(x_train, y_train)
                 self._update_weights()
 
-                # Calculate accuracy and loss of the trained network on train set.
-                accuracy, loss = self.score(x, y_true, True)
+                # Calculate accuracy of the trained network on test set.
+                accuracy, loss = self.score(x_test, y_test, True)
 
-                # TODO: Add calculation of accuracy and loss on test set.
+                accuracies_curr.append(accuracy)
+                losses_curr.append(loss)
 
-            # Store current accuracy.
-            self.accuracies.append(accuracy)
-            self.losses.append(loss)
-
-            if silent is False:
-                if i % 1 == 0:
+                if j % 10 == 0:
                     # Print current loss and accuracy of the neural net.
-                    print(f'Epoch: {i}, loss: {loss:.2f}, accuracy: {accuracy:.1f}%')
+                    print(f'Epoch: {j}, loss: {loss:.2f}, accuracy: {accuracy:.1f}%')
 
-        
+            # Calculate mean losses and accuracies and print them.
+            mean_accuracy = np.mean(accuracies_curr)
+            mean_loss = np.mean(losses_curr)
+            print(f'Mean accuracy on T{i}: {mean_accuracy:.2f}')
+            print(f'Mean losses on T{i}: {mean_loss:.2f}\n')
 
-    def calculate_accuracy(self, y_true, y_pred):
-        """ Calculate accuracy of the neural network.
+            accuracies.append(mean_accuracy)
+            losses.append(mean_loss)
 
-        Args:
-            y_true(numpy.ndarray): Array of true labels in one-hot format
-                                   and shape (num_of_inputs, one-hot).
-            y_pred(numpy.ndarray): Array of predicted labels in shape
-                                   (num_of_inputs, one-hot).
+        print(f'Accuracy list on Ti sets: {accuracies}')
+        print(f'Losses list on Ti sets: {losses}')
+        print(f'Mean accuracy on all T sets: {np.mean(accuracies):.2f}')
+        print(f'Mean losses on all T sets: {np.mean(losses):.2f}')
 
-        Returns:
-            accuracy(float): Accuracy of the neural net.
-        """
-        # Sum of correct predictions.
-        correct_pred = 0
-
-        for i in range(len(y_pred)):
-            # Boolean value wheter predicted label is the same as true label.
-            equal = np.equal(np.argmax(y_true[i]), np.argmax(y_pred[i]))
-
-            # Change boolean type to float and add
-            # it to sum of correct predictions.
-            correct_pred += equal.astype(float)
-
-        # Calculate accuracy.
-        accuracy = (correct_pred/len(y_true))*100
-
-        return accuracy
-
-    def score(self, x, y_true, forward=False):
-        """ Perform forward step, calculate accuracy and loss.
-
-        Args:
-            x(numpy.ndarray): Array of input data in shape
-                              (num_of_inputs, vector_input).
-            y_true(numpy.ndarray): Array of true labels in one-hot format
-                                   and shape (num_of_inputs, one-hot).
-
-        Returns:
-            accuracy(float): Accuracy of the neural net.
-        """
-
-        if forward:
-            # Perform forward step.
-            self._forward(x)
-
-        # Get y_pred values from memory.
-        y_pred = self.memory['a' + str(len(self.layers))]
-
-        # Calculate accuracy.
-        accuracy = self.calculate_accuracy(y_true, y_pred)
-
-        # Calculate loss.
-        cross_entropy_loss = np.sum(-y_true * np.log(y_pred))
-
-        return accuracy, cross_entropy_loss
-
-    def k_fold_validation(self, x, y_true, k, epochs, batch_size=256):
-        """ Perform k-fold cross validation.
+    def k_fold_validation_minibatch(self, x, y_true, k, epochs, batch_size=256):
+        """ Perform k-fold cross validation using minibatch.
 
         Args:
             x(numpy.ndarray): Array of input data in shape
@@ -306,7 +303,7 @@ class MLP():
                     accuracies_curr.append(accuracy)
                     losses_curr.append(loss)
 
-                if j % 5 == 0:
+                if j % 10 == 0:
                     # Print current loss and accuracy of the neural net.
                     print(f'Epoch: {j}, loss: {loss:.2f}, accuracy: {accuracy:.1f}%')
 
@@ -323,6 +320,63 @@ class MLP():
         print(f'Losses list on Ti sets: {losses}')
         print(f'Mean accuracy on all T sets: {np.mean(accuracies):.2f}')
         print(f'Mean losses on all T sets: {np.mean(losses):.2f}')
+
+    def calculate_accuracy(self, y_true, y_pred):
+        """ Calculate accuracy of the neural network.
+
+        Args:
+            y_true(numpy.ndarray): Array of true labels in one-hot format
+                                   and shape (num_of_inputs, one-hot).
+            y_pred(numpy.ndarray): Array of predicted labels in shape
+                                   (num_of_inputs, one-hot).
+
+        Returns:
+            accuracy(float): Accuracy of the neural net.
+        """
+        # Sum of correct predictions.
+        correct_pred = 0
+
+        for i in range(len(y_pred)):
+            # Boolean value wheter predicted label is the same as true label.
+            equal = np.equal(np.argmax(y_true[i]), np.argmax(y_pred[i]))
+
+            # Change boolean type to float and add it to sum of 
+            # correct predictions.
+            correct_pred += equal.astype(float)
+
+        # Calculate accuracy.
+        accuracy = (correct_pred/len(y_true))*100
+
+        return accuracy
+
+    def score(self, x, y_true, forward=False):
+        """ Perform forward step, calculate accuracy and loss.
+
+        Args:
+            x(numpy.ndarray): Array of input data in shape
+                              (num_of_inputs, vector_input).
+            y_true(numpy.ndarray): Array of true labels in one-hot format
+                                   and shape (num_of_inputs, one-hot).
+            forward: Wheter to compute forward step on given set.
+
+        Returns:
+            accuracy(float): Accuracy of the neural net.
+        """
+
+        if forward:
+            # Perform forward step.
+            self._forward(x)
+
+        # Get y_pred values from memory.
+        y_pred = self.memory['a' + str(len(self.layers))]
+
+        # Calculate accuracy.
+        accuracy = self.calculate_accuracy(y_true, y_pred)
+
+        # Calculate loss.
+        cross_entropy_loss = np.sum(-y_true * np.log(y_pred))
+
+        return accuracy, cross_entropy_loss
 
     def _forward(self, x):
         """ Perform forward step in the neural network.
@@ -417,22 +471,6 @@ class MLP():
             # Update w and b.
             self.param_values['w' + str(i)] -= self.lr * dcost_w
             self.param_values['b' + str(i)] -= self.lr * dcost_b.sum(axis=0)
-
-    def add_layer(self, input_dim, output_dim, activation):
-        """ Add layer to the neural network.
-
-        Args:
-            input_dim: Dimension of the input
-            output_dim: Dimension of the output
-            activation: Activation function
-        """
-
-        # Create new layer.
-        layer = {"input_dim": input_dim,
-                 "output_dim": output_dim,
-                 "activation": activation}
-
-        self.layers.append(layer)
 
     def print_layers(self):
         """ Print layers of the neural network. """
